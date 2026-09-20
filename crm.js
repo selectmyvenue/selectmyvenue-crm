161e8fe0946868578f3b72ea5a3362a60ddc6d7f
selectmyvenue <rajmba2010@gmail.com>
2026-09-21 04:55:19 +0530

    Publish latest matching core through CRM bootstrap

--- crm.js
+++ crm.js
@@ -525,7 +525,7 @@
     window.setTimeout(updateActionBadge, 1200);
   }
 
-  addScript("crm-base.js?v=20260921-automation-7", function () {
+  addScript("crm-base.js?v=20260921-automation-13", function () {
     let checks = 0;
     const waitForCore = window.setInterval(function () {
       checks += 1;
