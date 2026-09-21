6764452989ba841e18fd0cf1005e103d1777b669
selectmyvenue <rajmba2010@gmail.com>
2026-09-22 02:59:01 +0530

    Refresh CRM loader for combined automation 39-43

--- crm.js
+++ crm.js
@@ -525,7 +525,7 @@
     window.setTimeout(updateActionBadge, 1200);
   }
 
-  addScript("crm-base.js?v=20260922-automation-36", function () {
+  addScript("crm-base.js?v=20260922-automation-43", function () {
     let checks = 0;
     const waitForCore = window.setInterval(function () {
       checks += 1;
