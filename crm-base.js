6318875d435f2dc82993fb8ee723c386f28df4bf
selectmyvenue <rajmba2010@gmail.com>
2026-09-23 02:14:24 +0530

    Refresh core after final drag correction

--- crm-base.js
+++ crm-base.js
@@ -672,7 +672,7 @@
     observer.observe(tbody, { childList: true, subtree: true });
   }
 
-  loadScript("crm-core.js?v=20260923-clean-details-1", function () {
+  loadScript("crm-core.js?v=20260923-clean-details-2", function () {
     installInternalCommentOnlyBehavior();
     installLeadRenderNormalizer();
     installSaveCustomerCommentPreserver();
