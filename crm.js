5d75b5be38cada0a1ecdae16b94f58ed9fe0078d
selectmyvenue <rajmba2010@gmail.com>
2026-09-23 02:10:22 +0530

    Refresh base loader after Details cleanup

--- crm.js
+++ crm.js
@@ -525,7 +525,7 @@
     window.setTimeout(updateActionBadge, 1200);
   }
 
-  addScript("crm-base.js?v=20260923-details-dense-1", function () {
+  addScript("crm-base.js?v=20260923-clean-details-1", function () {
     let checks = 0;
     const waitForCore = window.setInterval(function () {
       checks += 1;
