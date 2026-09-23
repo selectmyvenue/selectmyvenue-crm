f19ed0fe92704696d06df4955375e747a58d5d22
selectmyvenue <rajmba2010@gmail.com>
2026-09-23 09:29:49 +0530

    Refresh CRM base after reassignment fix

--- crm.js
+++ crm.js
@@ -525,7 +525,7 @@
     window.setTimeout(updateActionBadge, 1200);
   }
 
-  addScript("crm-base.js?v=20260923-assign-fix-1", function () {
+  addScript("crm-base.js?v=20260923-reassign-rootfix-1", function () {
     let checks = 0;
     const waitForCore = window.setInterval(function () {
       checks += 1;
