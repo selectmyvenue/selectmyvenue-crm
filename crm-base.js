ea6f252230d768bd302981e6b89a0e9c9b06fce2
selectmyvenue <rajmba2010@gmail.com>
2026-09-24 04:13:20 +0530

    Refresh CRM caches for area and comment UI

--- crm-base.js
+++ crm-base.js
@@ -672,7 +672,7 @@
     observer.observe(tbody, { childList: true, subtree: true });
   }
 
-  loadScript("crm-core.js?v=20260923-venue-details-10", function () {
+  loadScript("crm-core.js?v=20260924-area-comments-1", function () {
     installInternalCommentOnlyBehavior();
     installLeadRenderNormalizer();
     installSaveCustomerCommentPreserver();
